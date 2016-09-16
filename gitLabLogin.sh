ac7bce33858abbd95b3027da96967be00ec8987c
Butler <2018abutler@local.tjhsst.edu>
2016-09-16 11:00:57 -0400

    first commit

---
+++ gitLabLogin.sh
@@ -0,0 +1,2 @@
+ssh -T git@gitlab.tjhsst.edu
+git config remote.origin.url git@gitlab.tjhsst.edu:2018abutler/$1.git
---
+++ gitLogin.sh
@@ -0,0 +1,2 @@
+ssh -T git@github.com
+git config remote.origin.url git@github.com:andman2000/$1.git
